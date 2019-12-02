3069b46adde69f26f2475cc84aa952747c5b8f0f
S Merchant <sohailsmerchant@gmail.com>
2019-12-02 23:00:53 +0000

    shell

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 clear
 
+# Path of the folder where corpus files are stored
+
 CORPUSPATH="/mnt/c/Development Work/KITAB/OpenITI"
 echo "Resetting local changes ..."
 echo $CORPUSPATH
@@ -12,6 +14,7 @@ ls | xargs -P10 -I{} git -C {} reset --hard
 echo "Fetching changes from corpus ..."
 ls | xargs -P10 -I{} git -C {} fetch origin 
 
+# Path of the folder where python script is located
 ROOTPATH="/mnt/c/Development Work/KITAB/kitab-metadata-automation"
 cd "$ROOTPATH"
 # exec /bin/bash
