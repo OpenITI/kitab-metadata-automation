2d8f2a8c2d63ebc7d2d36fb3d98917936eb4bcf2
KITAB Project <kitab-project@outlook.com>
2020-07-07 18:22:16 +0000

    output generated

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ clear
 #Path of the folder where corpus files are stored
 #echo "Enter the path to the corpus "
 CORPUSPATH="/home/admin-kitab/Documents/OpenITI/GitHub_clone"
-echo $CORPUSPATH
+#echo $CORPUSPATH
 
 cd $CORPUSPATH
 pwd
@@ -18,7 +18,7 @@ echo "Fetching changes from corpus ..."
 #Path of the folder where python script is located
 #echo "Enter the path to the script "
 ROOTPATH="/home/admin-kitab/Documents/Projects/kitab-metadata-automation"
-echo $ROOTPATH
+#echo $ROOTPATH
 
 cd $ROOTPATH
 pwd
@@ -27,6 +27,8 @@ echo "Generating corpus metadata  ..."
 python3 generate-metadata.py -c ./utility/config-automated-do-not-remove-or-change.py
 
 # echo "Committing changes to repo ..."
+git config user.email kitab-project@outlook.com
+git config user.name 'KITAB Project'
 git add .
 git commit -m 'output generated' 
 git push
