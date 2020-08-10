347c94f419ee556c731295cc293dbbdfa9965da8
pverkind <peter.verkinderen@gmail.com>
2020-08-10 09:39:33 +0200

    merge

---
+++ utility/config_RELEASE_2019_1.py
@@ -0,0 +1,20 @@
+corpus_path = r"G:\London\OpenITI\OpenITI_2019_1_1\data"
+exclude = (["OpenITI.github.io", "Annotation", "maintenance", "i.mech00",
+            "i.mech01", "i.mech02", "i.mech03", "i.mech04", "i.mech05",
+            "i.mech06", "i.mech07", "i.mech08", "i.mech09", "i.logic",
+            "i.cex", "i.cex_Temp", "i.mech", "i.mech_Temp", ".git"])
+data_in_25_year_repos = False
+perform_yml_check = False
+check_token_counts = False
+incl_char_length = False
+split_ar_lat = True
+output_path = r"G:/London/OpenITI/OpenITI_2019_1_1/"
+meta_tsv_fp = None
+meta_yml_fp = None
+meta_json_fp = None
+meta_header_fp = None
+passim_runs = [['October 2017 (V1)', 'passim1017'],
+               ['February 2019 (V2)', 'passim01022019'],
+               ['May 2019 (Aggregated)', 'aggregated01052019'],
+              ]
+silent = True
