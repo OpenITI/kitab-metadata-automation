f67cc0ce5ca8538d70bd6c0c7b3214f078846b65
pverkind <peter.verkinderen@gmail.com>
2021-01-11 10:43:54 +0100

    Add October passim run and remove Aggregated run

--- utility/config.py
+++ utility/config.py
@@ -36,8 +36,9 @@ meta_header_fp = None
 # List of lists (description, run_id on server):  
 passim_runs = [['October 2017 (V1)', 'passim1017'],
                ['February 2019 (V2)', 'passim01022019'],
-               ['May 2019 (Aggregated)', 'aggregated01052019'],
-               ['February 2020', 'passim01022020']]
+#               ['May 2019 (Aggregated)', 'aggregated01052019'],
+               ['February 2020', 'passim01022020'],
+               ['October 2020', 'passim01102020']]
 
 # Set to True to allow the script to make changes to yml files without asking:
 silent = False  # True/False
