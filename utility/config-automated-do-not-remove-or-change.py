761b02551b799e269f65f19b26f2179e197c91d1
KITAB Project <kitab-project@outlook.com>
2021-02-24 22:14:12 +0000

    output generated

--- utility/config-automated-do-not-remove-or-change.py
+++ utility/config-automated-do-not-remove-or-change.py
@@ -41,7 +41,9 @@ passim_runs = [['October 2017 (V1)', 'passim1017'],
                ['February 2019 (V2)', 'passim01022019'],
 #               ['May 2019 (Aggregated)', 'aggregated01052019'],
                ['February 2020', 'passim01022020'],
-               ['October 2020', 'passim01102020']]
+               ['October 2020', 'passim01102020'],
+               ['October 2020 (Aggregated)', 'aggregated01102020'],
+               ['Feb 2021', 'passim01022021']]
 
 # Set to True to allow the script to make changes to yml files without asking:
 silent = True  # True/False
