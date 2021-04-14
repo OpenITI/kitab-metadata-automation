fb5d0740bb5827352c14403ca24f320e8813b4d3
pverkind <peter.verkinderen@gmail.com>
2021-04-14 10:18:27 +0200

    Add Feb aggregated srt data

--- utility/config-automated-do-not-remove-or-change_wNoor.py
+++ utility/config-automated-do-not-remove-or-change_wNoor.py
@@ -36,7 +36,8 @@ meta_header_fp = None
 passim_runs = [['October 2017 (V1)', 'passim1017'],
                ['February 2019 (V2)', 'passim01022019'],
                ['May 2019 (Aggregated)', 'aggregated01052019'],
-               ['February 2020', 'passim01022020']]
+               ['Feb 2021', 'passim01022021'],
+               ['Feb 2021 (Aggregated)', 'aggregated01022021']]
 
 # Set to True to allow the script to make changes to yml files without asking:
 silent = True  # True/False
